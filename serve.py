e7d0a8ab26d3d0ca49806aaf70927be55419ade3
YL117 <128180180+YL117@users.noreply.github.com>
2023-05-11 22:49:29 -0400

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,22 @@
+import subprocess
+from flask import Flask, request
+import json
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def get_seed():
+    return socket.gethostname(), 200
+
+
+@app.route("/", methods=["POST"])
+def post_seed():
+    subprocess.Popen(["python", "stress_cpu.py"])
+    return "", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    print("App running on 0.0.0.0:5000")
